bff7534770f5dd75a8f0c50593f4c864d51dee1a
Ahmed bajry <bajry2003@gmail.com>
2026-06-17 08:03:39 +0300

    Start develop views

--- job/views.py
+++ job/views.py
@@ -4,7 +4,12 @@ from .models import Job
 # Create your views here.
 
 def job_list(request):
-    pass
+    job_list = Job.objects.all()
+    print(job_list)
+    context={
+        'jobs':job_list
+    }
+    return render(request,'job/job_list.html',context)
 
 
 def job_detail(reqwest,id):
